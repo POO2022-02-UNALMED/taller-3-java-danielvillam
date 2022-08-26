7d2e628c253973e07a62d4fbc508a86a9e43c815
danielvillam <55595656+danielvillam@users.noreply.github.com>
2022-08-25 20:51:15 -0500

    Subir el trabajo

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,42 @@
+package televisores;
+
+public class Control {
+	private TV tv;
+	
+	public void enlazar(TV tv) {
+		this.tv=tv;
+		tv.setControl(this);
+	}
+	
+	public TV getTV() {
+		return tv;
+	}
+	
+	public void turnOn() {
+		tv.turnOn();
+	}
+	
+	public void turnOff() {
+		tv.turnOff();
+	}
+	
+	public void canalUp() {
+		tv.canalUp();
+	}
+	
+	public void canalDown() {
+		tv.canalDown();
+	}
+	
+	public void volumenUp() {
+		tv.volumenUp();
+	}
+	
+	public void volumenDown() {
+		tv.volumenDown();
+	}
+	
+	public void setCanal(int canal) {
+		tv.setCanal(canal);
+	}
+}
---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,100 @@
+package televisores;
+
+public class TV {
+	private Marca marca;
+	private int canal;
+	private int precio;
+	private boolean estado;
+	private int volumen;
+	private Control control;
+	private static int numTV;
+	
+	public TV(Marca marca, boolean estado) {
+		this.marca=marca;
+		canal=1;
+		precio=500;
+		this.estado=estado;
+		volumen=1;
+		numTV++;
+	}
+	
+	public Marca getMarca() {
+		return marca;
+	}
+	
+	public void setMarca(Marca marca) {
+		this.marca=marca;
+	}
+	
+	public Control getControl() {
+		return control;
+	}
+	
+	public void setControl(Control control) {
+		this.control=control;
+	}
+	
+	public int getPrecio() {
+		return precio;
+	}
+	
+	public void setPrecio(int precio) {
+		this.precio=precio;
+	}
+	
+	public int getVolumen() {
+		return volumen;
+	}
+	
+	public void setVolumen(int volumen) {
+		this.volumen=volumen;
+	}
+	
+	public int getCanal() {
+		return canal;
+	}
+	
+	public void setCanal(int canal) {
+		this.canal=canal;
+	}
+	
+	public int numTV() {
+		return numTV;
+	}
+	
+	public void turnOn() {
+		estado=false;
+	}
+	
+	public void turnOff() {
+		estado=true;
+	}
+	
+	public boolean getEstado() {
+		return estado;
+	}
+	
+	public void canalUp() {
+		if(canal>=1 && canal>120 && estado==true) {
+			canal++;
+		}
+	}
+	
+	public void canalDown() {
+		if(canal<=120 && canal>1 && estado==true) {
+			canal--;
+		}
+	}
+	
+	public void volumenUp() {
+		if(volumen>=0 && canal>7 && estado==true) {
+			volumen++;
+		}
+	}
+	
+	public void volumenDown() {
+		if(canal<=7 && canal>0 && estado==true) {
+			volumen--;
+		}
+	}
+}
